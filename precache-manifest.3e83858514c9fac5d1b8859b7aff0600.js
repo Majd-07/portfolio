de8e47e067c8c3e21e38f8b2ea0aed9aa857ba34
Majd-07 <m.shoghri07@gmail.com>
2020-07-20 18:09:00 +0300

    Updates

---
+++ precache-manifest.3e83858514c9fac5d1b8859b7aff0600.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bfd861b8fdbda33970f6ee9ef211221c",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "9f82007cf93492105459",
+    "url": "/portfolio/static/css/main.28108a08.chunk.css"
+  },
+  {
+    "revision": "1d5f16a257a44b2b9a1d",
+    "url": "/portfolio/static/js/2.5c55d2a0.chunk.js"
+  },
+  {
+    "revision": "cf962f5a864269b39a366b9b575516e7",
+    "url": "/portfolio/static/js/2.5c55d2a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f82007cf93492105459",
+    "url": "/portfolio/static/js/main.ecacc6f8.chunk.js"
+  },
+  {
+    "revision": "f619aeccc94a0d089131",
+    "url": "/portfolio/static/js/runtime-main.075c0788.js"
+  },
+  {
+    "revision": "d892e6dd2cf903f53872ee794fe73ef7",
+    "url": "/portfolio/static/media/BG.d892e6dd.jpg"
+  },
+  {
+    "revision": "15dbd34f358e055f163a715368538f36",
+    "url": "/portfolio/static/media/DoDone.15dbd34f.png"
+  },
+  {
+    "revision": "1887140c812cb04f143770513c9ca0db",
+    "url": "/portfolio/static/media/avatar.1887140c.png"
+  },
+  {
+    "revision": "cd7b32903cb74721ecc40bac43041103",
+    "url": "/portfolio/static/media/web p1.cd7b3290.png"
+  },
+  {
+    "revision": "d72fb98056a435d542b0769c07455722",
+    "url": "/portfolio/static/media/web p2.d72fb980.png"
+  },
+  {
+    "revision": "f37ffa3a9af0457923c1baefe2abcf79",
+    "url": "/portfolio/static/media/web p3.f37ffa3a.png"
+  },
+  {
+    "revision": "c05c54f90f0cb496c3452687facf8aac",
+    "url": "/portfolio/static/media/web p4.c05c54f9.png"
+  },
+  {
+    "revision": "10ac47d08152614a8b356648cc3cbbba",
+    "url": "/portfolio/static/media/web p5.10ac47d0.png"
+  }
+]);

c294492f4bcc8af48a73fac868709a8bb601528f
Majd-07 <m.shoghri07@gmail.com>
2020-09-08 22:54:31 +0300

    add contact functionality

--- src/components/Contacts.jsx
+++ src/components/Contacts.jsx
@@ -2,7 +2,28 @@ import React from 'react'
 import { makeStyles, withStyles } from '@material-ui/core/styles'
 import { TextField, Typography, Button, Grid, Box } from '@material-ui/core'
 import SendIcon from '@material-ui/icons/Send'
+import emailjs from 'emailjs-com'
 
+function sendEmail(e) {
+	e.preventDefault()
+
+	emailjs
+		.sendForm(
+			'service_8il4qni',
+			'template_6ufp0wt',
+			e.target,
+			'user_gm8aCmr5gPbLgrM4ozgQo'
+		)
+		.then(
+			(result) => {
+				console.log(result.text)
+				alert('Your Message have been sent')
+			},
+			(error) => {
+				console.log(error.text)
+			}
+		)
+}
 const useStyles = makeStyles((theme) => ({
 	form: {
 		top: '50%',
@@ -44,7 +65,7 @@ const Contacts = () => {
 	return (
 		<Box component='div' style={{ background: '#1f2227', height: '100vh' }}>
 			<Grid container justify='center'>
-				<Box component='form' className={classes.form}>
+				<Box component='form' className={classes.form} onSubmit={sendEmail}>
 					<Typography
 						variant='h5'
 						style={{
@@ -57,6 +78,7 @@ const Contacts = () => {
 					<InputField
 						fullWidth={true}
 						label='Name'
+						name='user_name'
 						variant='outlined'
 						inputProps={{ style: { color: '#c5c2c2' } }}
 						margin='dense'
@@ -65,6 +87,7 @@ const Contacts = () => {
 					<InputField
 						fullWidth={true}
 						label='Email'
+						name='user_email'
 						variant='outlined'
 						inputProps={{ style: { color: '#c5c2c2' } }}
 						margin='dense'
@@ -72,17 +95,20 @@ const Contacts = () => {
 					<br />
 					<InputField
 						fullWidth={true}
-						label='Company name'
+						label='Message'
+						name='message'
 						variant='outlined'
 						inputProps={{ style: { color: '#c5c2c2' } }}
 						margin='dense'
-						size='medium'></InputField>
+						size='medium'
+						multiline></InputField>
 					<br />
 					<Button
 						className={classes.button}
 						//style={{ marginTop: '1rem', color: 'gold', borderColor: 'gold' }}
 						variant='outlined'
 						fullWidth={true}
+						type='submit'
 						endIcon={<SendIcon />}>
 						Contact Me
 					</Button>
